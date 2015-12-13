188ba03d7dca24bea241abfb718896077ae495cc
Adam Elnagger <adam.elnagger@gmail.com>
2015-12-12 19:24:56 -0500

    WIP render template.

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from bottle import route, run, template
+# The majority of this file is based off a guide by
+# Michael Lustfield.
+# You can find the original posting at https://michael.lustfield.net/nginx/bottle-uwsgi-nginx-quickstart
+from bottle import route, run, template, static_file
 
-@route('/hello/<name>')
-def index(name):
-	return template('<b>Hello {{name}}</b>!', name = name)
 
-run(host = 'localhost', port = 8080)
+# Really we ought to let the server handle this
+# but we probably want it for development.
+@route('/static/<filename:path>')
+def static(filename):
+		'''
+		Hand out static files.
+		'''
+		return static_file(filename, rool='./static')
+
+@route('/page/<page_name>')
+def show_page(page_name):
+	'''
+	Return a page which has been rendered
+	using a template.
+	'''
+	return template('page', page_name = page_name)
+
+if __name__ == '__main__':
+	run(
+		host = '0.0.0.0',
+		port = 8080)
